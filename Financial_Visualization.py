4e7954b24f9eab87b644d9f39cf91861cc3e80cd
Radithyam <107978691+Titowwww@users.noreply.github.com>
2024-05-07 12:37:48 +0700

    Update Financial_Visualization.py

--- Financial_Visualization.py
+++ Financial_Visualization.py
@@ -7,7 +7,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 # Mount Data
 # file_path = "Financial.csv"
 data = pd.read_csv("Financial.csv")
-
+data2 = pd.read_csv("salaries.csv")
 st.title('Financial Dashboard')
 
 # Bar Chart Country by Units Sold
@@ -19,10 +19,10 @@ st.pyplot()
 
 # Bar Chart Top 3 Job Title by Salary
 st.subheader("Top 3 Job Title by Salary")
-data_sorted = data.sort_values(by='salary', ascending=False)
-top_3 = data_sorted.head(3)
-st.bar_chart(top_3['salary'], top_3['job_title']) 
-st.title("Top 3 Salaries")
-st.xlabel('Job Title')
-st.ylabel('Salary')
+data_sorted = data2.sort_values(by='salary', ascending=False)
+top_3 = data_sorted.head(5)
+plt.bar(top_3['job_title'], top_3['salary'])
+plt.title("Bar Chart - Top 3 Salaries")
+plt.xlabel('Job Title')
+plt.ylabel('Salary')
 st.pyplot()
